f7f6317fd26d3177aba1ca1431727d126d94faec
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-04-24 13:10:50 +0200

    Bump version: 0.0.2 → 0.0.3-dev

--- src/compath_utils/__init__.py
+++ src/compath_utils/__init__.py
@@ -43,7 +43,7 @@ from compath_utils.exc import *
 from compath_utils.manager import *
 from compath_utils.models import *
 
-__version__ = '0.0.2'
+__version__ = '0.0.3-dev'
 
 __title__ = 'compath_utils'
 __description__ = "A utilities package for ComPath"
