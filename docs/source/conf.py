441a926ef2e54379af033d7dc698933b4d8bd295
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-05-02 13:28:58 +0200

    Bump version: 0.0.4-dev → 0.0.5-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ author = 'Charles Tapley Hoyt and Daniel Domingo-Fernández'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '0.0.4-dev'
+release = '0.0.5-dev'
 
 # The short X.Y version.
 parsed_version = re.match(
