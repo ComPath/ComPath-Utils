9f87908f447bd7b99b2e4a8be60a1da17f57fbac
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-07-30 12:26:00 +0200

    Fix failing test

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -26,15 +26,23 @@ class ManagerMissingPathway(ManagerMissingFunctions):
 
     def get_pathway_by_id(self, pathway_id):
         """Get a pathway by its database identifier."""
+        pass
 
     def get_pathway_names_to_ids(self):
         """Get a dictionary from pathway names to their identifiers."""
+        pass
 
     def populate(self, *args, **kwargs):
         """Populate the database."""
+        pass
+
+    def summarize(self):
+        """Summarize the database."""
+        pass
 
     def query_gene_set(self, gene_set):
         """Find pathways with genes in the given set."""
+        pass
 
 
 class ManagerMissingProtein(ManagerMissingPathway):
